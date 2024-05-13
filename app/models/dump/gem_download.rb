61191c71ad5c203bf9f7754c202915028a77e688
Samuel Giddins <segiddins@segiddins.me>
2024-05-13 16:39:32 +0900

    migrate

--- app/models/dump/gem_download.rb
+++ app/models/dump/gem_download.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: gem_downloads
+#
+#  id         :integer          not null, primary key
+#  count      :bigint
+#  rubygem_id :integer          not null
+#  version_id :integer          not null
+#
+# Indexes
+#
+#  index_gem_downloads_on_count                                (count)
+#  index_gem_downloads_on_rubygem_id_and_version_id            (rubygem_id,version_id) UNIQUE
+#  index_gem_downloads_on_version_id_and_rubygem_id_and_count  (version_id,rubygem_id,count)
+#
 class Dump::GemDownload < Dump::Record
   belongs_to :rubygem, class_name: "Dump::Rubygem"
   belongs_to :version, class_name: "Dump::Version"
--- config/routes.rb
+++ config/routes.rb
@@ -1,32 +1,38 @@
 # == Route Map
 #
-# I, [2024-04-16T16:46:29.414476 #83800]  INFO -- ddtrace: [ddtrace] DATADOG CONFIGURATION - PROFILING - {"profiling_enabled":false}
-# I, [2024-04-16T16:46:29.414653 #83800]  INFO -- ddtrace: [ddtrace] DATADOG CONFIGURATION - CORE - {"date":"2024-04-16T16:46:29-07:00","os_name":"arm64-apple-darwin23","version":"1.20.0","lang":"ruby","lang_version":"3.3.0","env":null,"service":"rake","dd_version":null,"debug":false,"tags":null,"runtime_metrics_enabled":false,"vm":"ruby-3.3.0","health_metrics_enabled":false}
-# I, [2024-04-16T16:46:30.325963 #83815]  INFO -- ddtrace: [ddtrace] DATADOG CONFIGURATION - PROFILING - {"profiling_enabled":false}
-# I, [2024-04-16T16:46:30.326156 #83815]  INFO -- ddtrace: [ddtrace] DATADOG CONFIGURATION - CORE - {"date":"2024-04-16T16:46:30-07:00","os_name":"arm64-apple-darwin23","version":"1.20.0","lang":"ruby","lang_version":"3.3.0","env":null,"service":"rails","dd_version":null,"debug":false,"tags":null,"runtime_metrics_enabled":false,"vm":"ruby-3.3.0","health_metrics_enabled":false}
-#                            Prefix Verb URI Pattern                                         Controller#Action
-#             version_import_errors GET  /version_import_errors(.:format)                    version_import_errors#index
-#                data_summary_index GET  /data_summary(.:format)                             data_summary#index
-#                          versions GET  /versions(.:format)                                 versions#index
-#                           version GET  /versions/:id(.:format)                             versions#show
-#                          raw_blob GET  /blobs/:sha256/raw(.:format)                        blobs#raw
-#                             blobs GET  /blobs(.:format)                                    blobs#index
-#                              blob GET  /blobs/:sha256(.:format)                            blobs#show
-#                      diff_rubygem GET  /rubygems/:name/diff(.:format)                      rubygems#diff
-#         diff_rubygem_file_history GET  /rubygems/:rubygem_name/file_history/diff(.:format) file_histories#diff
-#              rubygem_file_history GET  /rubygems/:rubygem_name/file_history(.:format)      file_histories#show
-#                          rubygems GET  /rubygems(.:format)                                 rubygems#index
-#                           rubygem GET  /rubygems/:name(.:format)                           rubygems#show
-#                           servers GET  /servers(.:format)                                  servers#index
-#                            server GET  /servers/:id(.:format)                              servers#show
-#                rails_health_check GET  /up(.:format)                                       rails/health#show
-#                              root GET  /                                                   servers#index
-#                          good_job      /good_job                                           GoodJob::Engine
-#                 maintenance_tasks      /maintenance_tasks                                  MaintenanceTasks::Engine
-#                           pg_hero      /pghero                                             PgHero::Engine
-#  turbo_recede_historical_location GET  /recede_historical_location(.:format)               turbo/native/navigation#recede
-#  turbo_resume_historical_location GET  /resume_historical_location(.:format)               turbo/native/navigation#resume
-# turbo_refresh_historical_location GET  /refresh_historical_location(.:format)              turbo/native/navigation#refresh
+# I, [2024-05-13T16:38:41.470504 #8155]  INFO -- ddtrace: [ddtrace] DATADOG CONFIGURATION - CORE - {"date":"2024-05-13T07:38:41Z","os_name":"arm64-apple-darwin23","version":"1.22.0","lang":"ruby","lang_version":"3.3.1","env":null,"service":"rake","dd_version":null,"debug":false,"tags":null,"runtime_metrics_enabled":false,"vm":"ruby-3.3.1","health_metrics_enabled":false,"profiling_enabled":false}
+# I, [2024-05-13T16:38:42.468563 #8371]  INFO -- ddtrace: [ddtrace] DATADOG CONFIGURATION - CORE - {"date":"2024-05-13T07:38:42Z","os_name":"arm64-apple-darwin23","version":"1.22.0","lang":"ruby","lang_version":"3.3.1","env":null,"service":"rails","dd_version":null,"debug":false,"tags":null,"runtime_metrics_enabled":false,"vm":"ruby-3.3.1","health_metrics_enabled":false,"profiling_enabled":false}
+#                            Prefix Verb   URI Pattern                                         Controller#Action
+#                     gem_downloads GET    /gem_downloads(.:format)                            gem_downloads#index
+#                                   POST   /gem_downloads(.:format)                            gem_downloads#create
+#                  new_gem_download GET    /gem_downloads/new(.:format)                        gem_downloads#new
+#                 edit_gem_download GET    /gem_downloads/:id/edit(.:format)                   gem_downloads#edit
+#                      gem_download GET    /gem_downloads/:id(.:format)                        gem_downloads#show
+#                                   PATCH  /gem_downloads/:id(.:format)                        gem_downloads#update
+#                                   PUT    /gem_downloads/:id(.:format)                        gem_downloads#update
+#                                   DELETE /gem_downloads/:id(.:format)                        gem_downloads#destroy
+#             version_import_errors GET    /version_import_errors(.:format)                    version_import_errors#index
+#                data_summary_index GET    /data_summary(.:format)                             data_summary#index
+#                          versions GET    /versions(.:format)                                 versions#index
+#                           version GET    /versions/:id(.:format)                             versions#show
+#                          raw_blob GET    /blobs/:sha256/raw(.:format)                        blobs#raw
+#                             blobs GET    /blobs(.:format)                                    blobs#index
+#                              blob GET    /blobs/:sha256(.:format)                            blobs#show
+#                      diff_rubygem GET    /rubygems/:name/diff(.:format)                      rubygems#diff
+#         diff_rubygem_file_history GET    /rubygems/:rubygem_name/file_history/diff(.:format) file_histories#diff
+#              rubygem_file_history GET    /rubygems/:rubygem_name/file_history(.:format)      file_histories#show
+#                          rubygems GET    /rubygems(.:format)                                 rubygems#index
+#                           rubygem GET    /rubygems/:name(.:format)                           rubygems#show
+#                           servers GET    /servers(.:format)                                  servers#index
+#                            server GET    /servers/:id(.:format)                              servers#show
+#                rails_health_check GET    /up(.:format)                                       rails/health#show
+#                              root GET    /                                                   servers#index
+#                          good_job        /good_job                                           GoodJob::Engine
+#                 maintenance_tasks        /maintenance_tasks                                  MaintenanceTasks::Engine
+#                           pg_hero        /pghero                                             PgHero::Engine
+#  turbo_recede_historical_location GET    /recede_historical_location(.:format)               turbo/native/navigation#recede
+#  turbo_resume_historical_location GET    /resume_historical_location(.:format)               turbo/native/navigation#resume
+# turbo_refresh_historical_location GET    /refresh_historical_location(.:format)              turbo/native/navigation#refresh
 #
 # Routes for GoodJob::Engine:
 #                root GET    /                                         redirect(301, path: jobs)
