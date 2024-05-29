852b317ad1bdb66854adea19ce6cad912761f7f9
Samuel Giddins <segiddins@segiddins.me>
2024-05-29 14:11:00 -0700

    remove unused include

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -4,7 +4,7 @@ class VersionsController < ApplicationController
   layout -> { ApplicationLayout }
 
   def show
-    version = Version.includes(:rubygem, :server, :version_data_entries, :metadata_blob, :package_blob, :version_import_error).strict_loading.find(params[:id])
+    version = Version.includes(:rubygem, :server, :metadata_blob, :package_blob, :version_import_error).strict_loading.find(params[:id])
     pagy, version_data_entries = pagy(version.version_data_entries.includes(:blob_excluding_contents), items: 1000)
     render Versions::ShowView.new(version:, version_data_entries:, pagy:)
   end
